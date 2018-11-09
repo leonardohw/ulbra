ae2df683d5b63c08e8b792b172111b26e20c446e
Leo <leonardoheliowolff@gmail.com>
2018-11-09 01:21:35 -0200

    aula 16 exercicio js em aula

---
+++ 2018-2/TNI/aula16 - javascript/script.js
@@ -0,0 +1,9 @@
+function mouseSobre() {
+	var divResultado = document.getElementById("resultado");
+	divResultado.innerHTML = divResultado.innerHTML + "mouse sobre. <br/>";
+};
+
+function mouseFora() {
+	var divResultado = document.getElementById("resultado");
+	divResultado.innerHTML = divResultado.innerHTML + "mouse fora. <br/>";
+};
